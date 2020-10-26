2c312b198b6c2406eba870dcb9fefd34cf6aed87
SS Bayesian <shankar@Swarnapradhas-iMac.local>
2020-10-26 16:28:19 +0530

    learning jsx basics

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 // Import React and ReactDOM libraries
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
 // Create a react component
-
+const App = () => {
+  return (
+    <div>
+        <label class="label" for="name">Enter name:</label>
+        <input id="name" type="text"/>
+        <buttton style = {{backgroundColor: 'blue'; color: 'white'}}>Submit</button>
+    </div>
+  );
+};
 
 //Take the react component and show it on the screen
-
-//Another dummy comment
-console.log('Hello Shankar')
+ReactDOM.render(<App />, document.querySelector("#root"));

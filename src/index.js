1a6a620e0be2614de8a5c7a6b8b10f3383ab5207
Shankar <Shankar@Shankars-MacBook-Air.local>
2020-10-31 21:55:03 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
 // Import React and ReactDOM libraries
 import React from "react";
 import ReactDOM from "react-dom";
+import PersonCard from "./PersonCard";
+import mira1 from "./images/mira-1.jpg";
+import mira2 from "./images/mira-2.jpg";
+import mikuchiku from "./images/miku_chiku-1.png";
 
 // Create a react component
 const App = () => {
   return (
     <div>
-        <label class="label" for="name">Enter name:</label>
-        <input id="name" type="text"/>
-        <buttton style = {{backgroundColor: 'blue'; color: 'white'}}>Submit</button>
+      <h1>hi shreya</h1>
+      <PersonCard
+        name="Mira"
+        image={mira2}
+        tagline="Royal detective"
+        description="Shreya's favourite character."
+      />
+      <PersonCard
+        name="Miku Chiku"
+        image={mikuchiku}
+        tagline="Mongooses"
+        description="Mira's funny partners."
+      />
     </div>
   );
 };
+
+console.log("Current directory: " + process.cwd());
 
 //Take the react component and show it on the screen
 ReactDOM.render(<App />, document.querySelector("#root"));
